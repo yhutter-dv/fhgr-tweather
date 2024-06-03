fab8bbd3ae15f2b7824e34a3c1251ff684006287
Yannick Hutter <hutteryannick@gmail.com>
2024-06-03 13:16:47 +0200

    Add CORS.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from analysis_settings.weather_location_repository import WeatherLocationRepository
 
 weather_location_repository = WeatherLocationRepository.init_from_file(
     "./scripts/cities.json"
 )
 app = FastAPI()
+app.add_middleware(
+       CORSMiddleware,
+       allow_origins=["*"],
+       allow_credentials=True,
+       allow_methods=["*"],
+       allow_headers=["*"],
+   )
 
 
 @app.get("/weather_locations")
