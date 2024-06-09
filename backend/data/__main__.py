27ca2c27c218c99d537a705738bbc3e756e53661
Yannick Hutter <hutteryannick@gmail.com>
2024-06-09 15:12:01 +0200

    Fix lint warnings due to WeatherMetricEnum.

--- backend/data/__main__.py
+++ backend/data/__main__.py
@@ -22,9 +22,9 @@ if __name__ == "__main__":
             location=location,
             date=request_date,
             metrics=[
-                WeatherMetricEnum.TEMPERATURE,
-                WeatherMetricEnum.RAIN,
-                WeatherMetricEnum.SNOWFALL,
+                WeatherMetricEnum(WeatherMetricEnum.TEMPERATURE),
+                WeatherMetricEnum(WeatherMetricEnum.RAIN),
+                WeatherMetricEnum(WeatherMetricEnum.SNOWFALL),
             ],
         )
         respone = api.make_request(request)
