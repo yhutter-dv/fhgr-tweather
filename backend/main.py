a3e0807dca283bc1399bfebc644bf90d222d0ea4
Yannick Hutter <hutteryannick@gmail.com>
2024-06-10 14:23:29 +0200

    Document Backend code.

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,7 @@ from analyze.weather_analyzer import WeatherAnalyzer
 from shared.weather_metric import WeatherMetric
 from shared.weather_metric_enum import WeatherMetricEnum
 
+# Create instances of needed classes
 weather_location_repository = WeatherLocationRepository()
 weather_analyzer = WeatherAnalyzer()
 metric_enums = [
@@ -29,6 +30,10 @@ app = FastAPI()
 
 @app.post("/weather_analyze")
 def weather_analyze(settings: WeatherAnalysisSettings) -> list[WeatherAnalysisResult]:
+    """Returns a list of Analysis Results according ot the Settings.
+
+    One AnalysisResult per Metric is returned.
+    """
     try:
         return weather_analyzer.analyze(settings=settings)
     except Exception as e:
@@ -37,15 +42,21 @@ def weather_analyze(settings: WeatherAnalysisSettings) -> list[WeatherAnalysisRe
 
 @app.get("/weather_locations")
 def weather_locations() -> list[str]:
+    """Returns a list of supported Locations.
+
+    Please note that currently only locations in Switzerland are supported.
+    """
     return weather_location_repository.get_location_names()
 
 
 @app.get("/weather_metrics")
 def weather_metrics() -> list[WeatherMetric]:
+    """Returns a list of supported Weather Metrics"""
     return metrics
 
 
 # Enable CORS
+# It is important that this is done AFTER defining the endpoints.
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
