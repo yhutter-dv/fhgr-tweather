cca0417c75e7e6ac6114d5f3f2111d5cad41eab6
Yannick Hutter <hutteryannick@gmail.com>
2024-06-10 09:46:00 +0200

    Add tests for Weahter Api.

--- backend/data/test_weather_api.py
+++ backend/data/test_weather_api.py
@@ -7,9 +7,10 @@ from shared.weather_metric_enum import WeatherMetricEnum
 
 
 class TestWeatherApi(unittest.TestCase):
-    def test_weather_api_valid_request(self):
-        api = WeatherApi()
+    def setUp(self) -> None:
+        self.api = WeatherApi()
 
+    def test_weather_api_valid_request(self):
         request_location = WeatherLocation(
             name="Buchs SG",
             postal_code=9470,
@@ -23,22 +24,22 @@ class TestWeatherApi(unittest.TestCase):
         request = WeatherDataRequest(
             location=request_location, date=request_date, metrics=request_metrics
         )
-        response = api.make_request(request)
+        response = self.api.make_request(request)
         self.assertIsNotNone(response)
         self.assertFalse(response.has_error)
         self.assertEqual("", response.error_reason)
         self.assertTrue(len(response.values) > 0)
         self.assertIsNotNone(response.values[0])
 
-    def test_weather_api_invalid_request(self):
-        api = WeatherApi()
-
+    def test_weather_api_invalid_request_date_in_past(self):
         request_location = WeatherLocation(
             name="Buchs SG",
             postal_code=9470,
             latitude=47.16516222572188,
             longitude=9.475764283520503,
         )
+
+        # Request should be invalid because the date is to far in the past.
         request_date = date.today() - timedelta(days=365)
 
         request_metrics = [WeatherMetricEnum(WeatherMetricEnum.RAIN)]
@@ -46,7 +47,29 @@ class TestWeatherApi(unittest.TestCase):
         request = WeatherDataRequest(
             location=request_location, date=request_date, metrics=request_metrics
         )
-        response = api.make_request(request)
+        response = self.api.make_request(request)
+        self.assertIsNotNone(response)
+        self.assertTrue(response.has_error)
+        self.assertNotEqual("", response.error_reason)
+        self.assertTrue(len(response.values) == 0)
+
+    def test_weather_api_invalid_request_date_in_future(self):
+        request_location = WeatherLocation(
+            name="Buchs SG",
+            postal_code=9470,
+            latitude=47.16516222572188,
+            longitude=9.475764283520503,
+        )
+
+        # Request should be invalid because the date is to far in the future.
+        request_date = date.today() + timedelta(days=365)
+
+        request_metrics = [WeatherMetricEnum(WeatherMetricEnum.RAIN)]
+
+        request = WeatherDataRequest(
+            location=request_location, date=request_date, metrics=request_metrics
+        )
+        response = self.api.make_request(request)
         self.assertIsNotNone(response)
         self.assertTrue(response.has_error)
         self.assertNotEqual("", response.error_reason)
